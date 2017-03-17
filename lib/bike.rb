fbe3e52677cba954703b36842eee15310e0096e9
Alex Jukes <alexjukes@Alexs-MacBook-Pro-2.local>
2017-03-17 12:07:57 +0000

    Added ability to report broken bikes

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,11 @@
 class Bike
+ # Currently redundant code 
   def working?
     true
   end
+
+  def report_broken
+    @broken = true
+  end
+  attr_reader :broken
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,8 +1,10 @@
 require './lib/bike.rb'
 
 describe Bike do
-it 'should respond_to "working?" function.' do
-  expect(subject).to respond_to :working?
-end
-  #it { is_expected.to respond_to :working? }
+  it { is_expected.to respond_to :report_broken }
+
+  it 'checks that the report status of a bike that has been reported broken is set to broken' do
+    subject.report_broken
+    expect(subject.broken).to eq true
+  end
 end
