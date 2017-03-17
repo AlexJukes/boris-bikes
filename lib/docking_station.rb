a6b10080ab4d202b0257973a49f3e4f73925e5da
Stephanie Crampin <stephcrampin@Stephanies-MacBook-Pro.local>
2017-03-17 10:51:01 +0000

    completed ex 15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,29 @@
+require './lib/bike.rb'
+
 class DockingStation
   #We initialize the class and set the bikes variable to be an empty Array
     def initialize
       @bikes = []
     end
   def release_bike
-    raise "No bikes available." if @bikes.empty? # Guard condition
-    @bikes.pop
+    raise "No bikes available." if empty? # Guard condition
+    bikes.pop
   end
 
   def dock_bike(bike_56)
-      raise "Docking station is full" unless @bikes.length < 20
-      @bikes << bike_56
+      raise "Docking station is full" if full?
+      bikes << bike_56
   end
   attr_reader :bikes
+
+  private
+
+  def full?
+    bikes.length >= 20
+  end
+
+  def empty?
+    bikes.empty?
+  end
+
 end
